eb519146651be844b513a30bceae6114ded5aeb0
ashbyp <ashbyp@yahoo.co.uk>
2019-03-30 11:12:51 +0000

    added player V5

--- cards/cribbage/player.py
+++ cards/cribbage/player.py
@@ -4,7 +4,7 @@ from cards.base.card import Card, standard_deck
 from cards.cribbage import score
 
 
-def choose_best_hand_ignore_box(hand, hand_size):
+def best_hand_ignore_box(hand, hand_size):
     best = []
 
     for comb in itertools.combinations(hand, hand_size):
@@ -20,7 +20,7 @@ def choose_best_hand_ignore_box(hand, hand_size):
     return best
 
 
-def choose_best_hand(hand, hand_size, my_box):
+def best_hand_count_box(hand, hand_size, my_box):
     best = []
 
     for comb in itertools.combinations(hand, hand_size):
@@ -41,6 +41,116 @@ def choose_best_hand(hand, hand_size, my_box):
             best.append((sc, comb, box))
 
     return best
+
+
+def best_average_hand_ignore_box(hand, hand_size, deck):
+    # for each possible set of 4 cards in the hand
+    #   find score for each possible turn card
+    #   average the scores
+    #   choose hand with highest average
+    # this method sucks in sim mode, ok for interactive
+
+    possible_turns = [x for x in deck if x not in hand]
+    hand_scores = []
+
+    for comb in itertools.combinations(hand, hand_size):
+        scores = []
+        for turn in possible_turns:
+            scores.append(score.score_hand(list(comb), turn))
+        hand_scores.append((comb, scores))
+
+    best_score = 0
+    best_hand = None
+    for comb, average_score in [(comb, sum(scores) / len(scores)) for comb, scores in hand_scores]:
+        if average_score > best_score:
+            best_score = average_score
+            best_hand = comb
+
+    return best_hand
+
+
+def best_average_hand_count_box(hand, hand_size, deck, my_box):
+    # for each possible set of 4 cards in the hand
+    #   find score for each possible turn card
+    #   average the scores
+    #   choose hand with highest average
+    # this method sucks in sim mode, ok for interactive
+
+    possible_turns = [x for x in deck if x not in hand]
+    hand_scores = []
+
+    for comb in itertools.combinations(hand, hand_size):
+        box = [x for x in hand if x not in comb]
+        box_sc = 2 if (box[0].rank == box[1].rank or box[0].value + box[1].value == 15) else 0
+        scores = []
+        for turn in possible_turns:
+            hand_score = score.score_hand(list(comb), turn)
+            if my_box:
+                hand_score += box_sc
+            else:
+                hand_score -= box_sc
+            scores.append(hand_score)
+        hand_scores.append((comb, scores))
+
+    best_score = 0
+    best_hand = None
+    for comb, average_score in [(comb, sum(scores) / len(scores)) for comb, scores in hand_scores]:
+        if average_score > best_score:
+            best_score = average_score
+            best_hand = comb
+
+    return best_hand
+
+
+def best_peg_card(stack, hand):
+    stack_score = sum([x.value for x in stack])
+    score_per_card = []
+    for card in hand:
+        if stack_score + card.value <= 31:
+            score_per_card.append(score.score_pegging_stack(stack + [card])[0])
+        else:
+            score_per_card.append(None)
+
+    if all(x is None for x in score_per_card):
+        return None
+
+    best_score = max(x for x in score_per_card if x is not None)
+
+    for i, s in enumerate(score_per_card):
+        if s == best_score:
+            return hand[i]
+
+    return None
+
+
+def best_peg_cards_skip_5s_and_21s(stack, hand):
+    stack_score = sum([x.value for x in stack])
+    score_per_card = []  # tuples (score, stack_score)
+    for card in hand:
+        if stack_score + card.value <= 31:
+            score_per_card.append((score.score_pegging_stack(stack + [card])[0], stack_score + card.value))
+        else:
+            score_per_card.append((None, None))
+
+    if all(x[0] is None for x in score_per_card):
+        return None
+
+    best_score = max(x[0] for x in score_per_card if x[0] is not None)
+
+    possible_plays = []
+
+    for i, s in enumerate(score_per_card):
+        if s[0] == best_score:
+            possible_plays.append((s[1], hand[i]))
+
+    if possible_plays:
+        best_possible_plays = [x for x in possible_plays if x[0] not in (5, 21)]
+        if best_possible_plays:
+            return best_possible_plays[0][1]
+        else:
+            return possible_plays[0][1]
+
+    return None
 
 
 class Player:
@@ -177,28 +287,11 @@ class ComputerPlayerV1(Player):
             super().__init__(name)
 
     def choose_discards(self, hand, my_box):
-        best = choose_best_hand_ignore_box(hand, 4)[0][1]
+        best = best_hand_ignore_box(hand, 4)[0][1]
         return [x for x in hand if x not in best]
 
     def next_pegging_card(self, stack, hand, turn_card):
-        stack_score = sum([x.value for x in stack])
-        score_per_card = []
-        for card in hand:
-            if stack_score + card.value <= 31:
-                score_per_card.append(score.score_pegging_stack(stack + [card])[0])
-            else:
-                score_per_card.append(None)
-
-        if all(x is None for x in score_per_card):
-            return None
-
-        best_score = max(x for x in score_per_card if x is not None)
-
-        for i, s in enumerate(score_per_card):
-            if s == best_score:
-                return hand[i]
-
-        return None
+        return best_peg_card(stack, hand)
 
     def strategy(self):
         return "I will discard two cards that leave the best score in my hand, I will not consider the score of\n" +\
@@ -206,53 +299,7 @@ class ComputerPlayerV1(Player):
                " me the best score, or if all equal, then a random card"
 
 
-class ComputerPlayerV2(ComputerPlayerV1):
-    ME_COUNT = 1
-
-    def __init__(self, name=None):
-        if not name:
-            super().__init__(f'CompV2_{ComputerPlayerV2.ME_COUNT}')
-            ComputerPlayerV2.ME_COUNT += 1
-        else:
-            super().__init__(name)
-
-    def next_pegging_card(self, stack, hand, turn_card):
-        stack_score = sum([x.value for x in stack])
-        score_per_card = []  # tuples (score, stack_score)
-        for card in hand:
-            if stack_score + card.value <= 31:
-                score_per_card.append((score.score_pegging_stack(stack + [card])[0], stack_score + card.value))
-            else:
-                score_per_card.append((None, None))
-
-        if all(x[0] is None for x in score_per_card):
-            return None
-
-        best_score = max(x[0] for x in score_per_card if x[0] is not None)
-
-        possible_plays = []
-
-        for i, s in enumerate(score_per_card):
-            if s[0] == best_score:
-                possible_plays.append((s[1], hand[i]))
-
-        if possible_plays:
-            best_possible_plays = [x for x in possible_plays if x[0] not in (5, 21)]
-            if best_possible_plays:
-                return best_possible_plays[0][1]
-            else:
-                return possible_plays[0][1]
-
-        return None
-
-    def strategy(self):
-        return "I will discard two cards that leave the best score in my hand, I will not consider the score of\n" +\
-               " the cards thrown into the box or the turn card.  When pegging, I will play the card that gives\n" +\
-               " me the best score, or if all equal, then a random car and will prefer not to leave a stack count\n" +\
-               " of 5 or 21"
-
-
-class ComputerPlayerV3(ComputerPlayerV2):
+class ComputerPlayerV2(Player):
     ME_COUNT = 1
 
     def __init__(self, name=None):
@@ -263,8 +310,35 @@ class ComputerPlayerV3(ComputerPlayerV2):
             super().__init__(name)
 
     def choose_discards(self, hand, my_box):
-        best = choose_best_hand(hand, 4, my_box)[0][1]
+        best = best_hand_ignore_box(hand, 4)[0][1]
         return [x for x in hand if x not in best]
+
+    def next_pegging_card(self, stack, hand, turn_card):
+        return best_peg_cards_skip_5s_and_21s(stack, hand)
+
+    def strategy(self):
+        return "I will discard two cards that leave the best score in my hand, I will not consider the score of\n" +\
+               " the cards thrown into the box or the turn card.  When pegging, I will play the card that gives\n" +\
+               " me the best score, or if all equal, then a random car and will prefer not to leave a stack count\n" +\
+               " of 5 or 21"
+
+
+class ComputerPlayerV3(Player):
+    ME_COUNT = 1
+
+    def __init__(self, name=None):
+        if not name:
+            super().__init__(f'CompV3_{ComputerPlayerV3.ME_COUNT}')
+            ComputerPlayerV2.ME_COUNT += 1
+        else:
+            super().__init__(name)
+
+    def choose_discards(self, hand, my_box):
+        best = best_hand_count_box(hand, 4, my_box)[0][1]
+        return [x for x in hand if x not in best]
+
+    def next_pegging_card(self, stack, hand, turn_card):
+        return best_peg_cards_skip_5s_and_21s(stack, hand)
 
     def strategy(self):
         return "I will discard two cards that leave the best score in my hand, taking account of the score of\n" +\
@@ -273,12 +347,12 @@ class ComputerPlayerV3(ComputerPlayerV2):
                " of 5 or 21"
 
 
-class ComputerPlayerV4(ComputerPlayerV3):
+class ComputerPlayerV4(Player):
     ME_COUNT = 1
 
     def __init__(self, name=None):
         if not name:
-            super().__init__(f'CompV3_{ComputerPlayerV4.ME_COUNT}')
+            super().__init__(f'CompV4_{ComputerPlayerV4.ME_COUNT}')
             ComputerPlayerV4.ME_COUNT += 1
         else:
             super().__init__(name)
@@ -286,29 +360,11 @@ class ComputerPlayerV4(ComputerPlayerV3):
         self._deck = standard_deck()
 
     def choose_discards(self, hand, my_box):
-        # for each possible set of 4 cards in the hand
-        #   find score for each possible turn card
-        #   average the scores
-        #   choose hand with highest average
-        # this method sucks in sim mode, ok for interactive
+        best = best_average_hand_ignore_box(hand, 4, self._deck)
+        return [x for x in hand if x not in best]
 
-        possible_turns = [x for x in self._deck if x not in hand]
-        hand_scores = []
-
-        for comb in itertools.combinations(hand, 4):
-            scores = []
-            for turn in possible_turns:
-                scores.append(score.score_hand(list(comb), turn))
-            hand_scores.append((comb, scores))
-
-        best_score = 0
-        best_hand = None
-        for comb, average_score in [(comb, sum(scores) / len(scores)) for comb, scores in hand_scores]:
-            if average_score > best_score:
-                best_score = average_score
-                best_hand = comb
-
-        return [x for x in hand if x not in best_hand]
+    def next_pegging_card(self, stack, hand, turn_card):
+        return best_peg_cards_skip_5s_and_21s(stack, hand)
 
     def strategy(self):
         return "I will evaluate all possible hands with all possible turn cards, and discard the two cards that\n" +\
@@ -317,3 +373,33 @@ class ComputerPlayerV4(ComputerPlayerV3):
                 " then a random card; and will prefer not to leave a stack count of 5 or 21"
 
 
+class ComputerPlayerV5(Player):
+    ME_COUNT = 1
+
+    def __init__(self, name=None):
+        if not name:
+            super().__init__(f'CompV5_{ComputerPlayerV5.ME_COUNT}')
+            ComputerPlayerV4.ME_COUNT += 1
+        else:
+            super().__init__(name)
+
+        self._deck = standard_deck()
+
+    def choose_discards(self, hand, my_box):
+        best = best_average_hand_count_box(hand, 4, self._deck, my_box)
+        return [x for x in hand if x not in best]
+
+    def next_pegging_card(self, stack, hand, turn_card):
+        return best_peg_cards_skip_5s_and_21s(stack, hand)
+
+    def strategy(self):
+        return "I will evaluate all possible hands with all possible turn cards, and discard the two cards that\n" +\
+                " gives me the highest average hand score, I will not consider the score of the cards thrown into\n" +\
+                " the box.  When pegging, I will play the card that gives me the best score, or if all equal\n" +\
+                " then a random card; and will prefer not to leave a stack count of 5 or 21"
+
+
+if __name__ == '__main__':
+    p = ComputerPlayerV5()
+    print(p.choose_discards(Card.from_str_list('ad,2d,3d,4d,6h,6d'), False))
+    print(p.choose_discards(Card.from_str_list('ad,2d,3d,4d,5d,6d'), True))
