86a202b325f6fc9292aa4773122c027e51dedfcc
ashbyp <ashbyp@yahoo.co.uk>
2022-05-06 16:38:00 +0100

    poker hand eval

---
+++ cards/poker/score.py
@@ -0,0 +1,91 @@
+import itertools
+from urllib.request import urlopen
+from cards.base.card import Card
+
+
+def eval_desc(result, verbose=False):
+
+    value_dict = {11: 'Jack', 12: 'Queen', 13: 'King', 14: 'Ace'}
+
+    res_code = {
+        8: ('Straight Flush', lambda x: f'{value_dict.get(x[1], str(x[1]))} high'),
+        7: ('Four of a Kind', lambda x: f'{value_dict.get(x[1], str(x[1]))}\'s'),
+        6: ('Full House', lambda x: f'{value_dict.get(x[1][0], str(x[1][0]))}\'s over {value_dict.get(x[2][0], str(x[2][0]))}\'s'),
+        5: ('Flush', lambda x: f'{value_dict.get(x[1][0], str(x[1][0]))} high'),
+        4: ('Straight', lambda x: f'{value_dict.get(x[1], str(x[1]))} high'),
+        3: ('Three of a Kind', lambda x: f'{value_dict.get(x[1][0], str(x[1][0]))}\'s'),
+        2: ('Two pair', lambda x: f'{value_dict.get(x[1][0], str(x[1][0]))}\'s and {value_dict.get(x[1][1], str(x[1][1]))}\'s'),
+        1: ('One pair', lambda x: f'{value_dict.get(x[1][0], str(x[1][0]))}\'s'),
+        0: ('High card', lambda x: f'{value_dict.get(x[2][0], str(x[2][0]))}'),
+    }
+    return res_code[result[0]][0] if not verbose else f'{res_code[result[0]][0]} ({res_code[result[0]][1](result)})'
+
+
+def eval_hand(hand):
+    # Return ranking followed by tie-break information.
+    # 8. Straight Flush
+    # 7. Four of a Kind
+    # 6. Full House
+    # 5. Flush
+    # 4. Straight
+    # 3. Three of a Kind
+    # 2. Two pair
+    # 1. One pair
+    # 0. High card
+    #
+    # Aces are transformed to 14 in the tie break info to allow hands to be ranked correctly
+    if len(hand) != 5:
+        raise ValueError("Can only evaluate 5 card hands")
+
+    values = sorted([c.rank if c.rank != 1 else 14 for c in hand], reverse=True)
+    suits = [c.suit for c in hand]
+    straight = (values == list(range(values[0], values[0]-5, -1))
+                or values == [14, 5, 4, 3, 2])
+    flush = all(s == suits[0] for s in suits)
+
+    if straight and flush:
+        return 8, values[0] if values[1] != 5 else 5
+    if flush:
+        return 5, values
+    if straight:
+        return 4, values[0] if values[1] != 5 else values[1]
+
+    trips = []
+    pairs = []
+    for v, group in itertools.groupby(values):
+        count = sum(1 for _ in group)
+        if count == 4: return 7, v, values
+        elif count == 3: trips.append(v)
+        elif count == 2: pairs.append(v)
+
+    if trips:
+        return (6 if pairs else 3), trips, pairs, values
+    return len(pairs), pairs, values
+
+
+def score_some_hands():
+    data = urlopen('https://projecteuler.net/project/resources/p054_poker.txt')
+    for line in data:
+        try:
+            cards = Card.from_str_list(line.decode('utf-8').strip(), sep=' ')
+            hand1 = cards[0:5]
+            hand2 = cards[5:]
+            eval1 = eval_hand(hand1)
+            eval2 = eval_hand(hand2)
+            desc1 = eval_desc(eval1, True)
+            desc2 = eval_desc(eval2, True)
+            result = "draws with"
+            if eval1 > eval2:
+                result = 'beats'
+            elif eval1 < eval2:
+                result = 'loses to'
+
+            print(f'{hand1} {desc1} {result} {hand2} {desc2}')
+        except ValueError as e:
+            print(e, f'input was {line}')
+            raise e
+
+
+if __name__ == '__main__':
+    score_some_hands()
+
---
+++ cards/poker/tests/test_score.py
@@ -0,0 +1,112 @@
+from cards.poker.score import eval_desc, eval_hand
+from cards.base.card import Card
+from unittest import TestCase
+
+
+class TestCard(TestCase):
+
+    def setUp(self):
+        self.straight_flush = Card.from_str_list("10d jd qd kd ad", sep=' ')
+        self.four_of_a_kind = Card.from_str_list("10d 10s 10c 10h ad", sep=' ')
+        self.full_house = Card.from_str_list("10d 10s 10s kd ks", sep=' ')
+        self.flush = Card.from_str_list("8d jd qd kd ad", sep=' ')
+        self.straight = Card.from_str_list("10s jd qd kd ad", sep=' ')
+        self.three_of_a_kind = Card.from_str_list("10d 10s 10c kd ad", sep=' ')
+        self.two_pair = Card.from_str_list("10d 10s qd qs ad", sep=' ')
+        self.one_pair = Card.from_str_list("10d 10s qd kd ad", sep=' ')
+        self.high_card = Card.from_str_list("8s jd qd kd ad", sep=' ')
+
+    def test_hands_are_recognised(self):
+        self.assertEqual(eval_desc(eval_hand(self.straight_flush)), "Straight Flush")
+        self.assertEqual(eval_desc(eval_hand(self.four_of_a_kind)), "Four of a Kind")
+        self.assertEqual(eval_desc(eval_hand(self.full_house)), "Full House")
+        self.assertEqual(eval_desc(eval_hand(self.flush)), "Flush")
+        self.assertEqual(eval_desc(eval_hand(self.straight)), "Straight")
+        self.assertEqual(eval_desc(eval_hand(self.three_of_a_kind)), "Three of a Kind")
+        self.assertEqual(eval_desc(eval_hand(self.two_pair)), "Two pair")
+        self.assertEqual(eval_desc(eval_hand(self.one_pair)), "One pair")
+        self.assertEqual(eval_desc(eval_hand(self.high_card)), "High card")
+
+    def test_verbose_descriptions(self):
+        self.assertEqual(eval_desc(eval_hand(self.straight_flush), verbose=True), "Straight Flush (Ace high)")
+        self.assertEqual(eval_desc(eval_hand(self.four_of_a_kind), verbose=True), "Four of a Kind (10's)")
+        self.assertEqual(eval_desc(eval_hand(self.full_house), verbose=True), "Full House (10's over King's)")
+        self.assertEqual(eval_desc(eval_hand(self.flush), verbose=True), "Flush (Ace high)")
+        self.assertEqual(eval_desc(eval_hand(self.straight), verbose=True), "Straight (Ace high)")
+        self.assertEqual(eval_desc(eval_hand(self.three_of_a_kind), verbose=True), "Three of a Kind (10's)")
+        self.assertEqual(eval_desc(eval_hand(self.two_pair), verbose=True), "Two pair (Queen's and 10's)")
+        self.assertEqual(eval_desc(eval_hand(self.one_pair), verbose=True), "One pair (10's)")
+        self.assertEqual(eval_desc(eval_hand(self.high_card), verbose=True), "High card (Ace)")
+
+    def test_return_values(self):
+        self.assertEqual(eval_hand(self.straight_flush), (8, 14))
+        self.assertEqual(eval_hand(self.four_of_a_kind), (7, 10, [14, 10, 10, 10, 10]))
+        self.assertEqual(eval_hand(self.full_house), (6, [10], [13], [13, 13, 10, 10, 10]))
+        self.assertEqual(eval_hand(self.flush), (5, [14, 13, 12, 11, 8]))
+        self.assertEqual(eval_hand(self.straight), (4, 14))
+        self.assertEqual(eval_hand(self.three_of_a_kind), (3, [10], [], [14, 13, 10, 10, 10]))
+        self.assertEqual(eval_hand(self.two_pair), (2, [12, 10], [14, 12, 12, 10, 10]))
+        self.assertEqual(eval_hand(self.one_pair), (1, [10], [14, 13, 12, 10, 10]))
+        self.assertEqual(eval_hand(self.high_card), (0, [], [14, 13, 12, 11, 8]))
+
+    def test_rankings(self):
+        self.assertTrue(
+            eval_hand(self.straight_flush) >
+            eval_hand(self.four_of_a_kind) >
+            eval_hand(self.full_house) >
+            eval_hand(self.flush) >
+            eval_hand(self.straight) >
+            eval_hand(self.three_of_a_kind) >
+            eval_hand(self.two_pair) >
+            eval_hand(self.one_pair) >
+            eval_hand(self.high_card)
+        )
+
+    def test_equal_hands(self):
+        straight1 = Card.from_str_list("10s jd qd kd ad", sep=' ')
+        straight2 = Card.from_str_list("10c js qs ks as", sep=' ')
+        self.assertEqual(eval_hand(straight1), eval_hand(straight2))
+
+    def test_straight_tie_breaker(self):
+        straight1 = Card.from_str_list("9s 10d jd qd kd", sep=' ')
+        straight2 = Card.from_str_list("10c js qs ks as", sep=' ')
+
+        self.assertEqual(eval_desc(eval_hand(straight1)), "Straight")
+        self.assertEqual(eval_desc(eval_hand(straight2)), "Straight")
+
+        self.assertNotEqual(eval_hand(straight1), eval_hand(straight2))
+        self.assertGreater(straight2, straight1)
+
+    def test_pair_tie_breaker(self):
+        pair1 = Card.from_str_list("9s 9d jd qd kd", sep=' ')
+        pair2 = Card.from_str_list("10c 10s qs ks as", sep=' ')
+
+        self.assertEqual(eval_desc(eval_hand(pair1)), "One pair")
+        self.assertEqual(eval_desc(eval_hand(pair2)), "One pair")
+
+        self.assertNotEqual(eval_hand(pair1), eval_hand(pair2))
+        self.assertGreater(pair2, pair1)
+
+    def test_ace_handling(self):
+        ace_low = Card.from_str_list("1s 2d 3d 4d 5d", sep=' ')
+        ace_high = Card.from_str_list("10c js qs ks as", sep=' ')
+
+        self.assertEqual(eval_desc(eval_hand(ace_low)), "Straight")
+        self.assertEqual(eval_desc(eval_hand(ace_high)), "Straight")
+
+        self.assertNotEqual(eval_hand(ace_low), eval_hand(ace_high))
+        self.assertGreater(eval_hand(ace_high), eval_hand(ace_low))
+
+    def test_input_conditions(self):
+        with self.assertRaises(ValueError):
+            eval_hand(Card.from_str_list("1s 2d 5d", sep=' '))
+
+    def test_some_straights(self):
+        self.assertTrue(
+            eval_hand(Card.from_str_list("10s js qs ks as", sep=' ')) >
+            eval_hand(Card.from_str_list("9s 10s js qs ks", sep=' ')) >
+            eval_hand(Card.from_str_list("As 2s 3s 4s 5s", sep=' ')) >
+            eval_hand(Card.from_str_list("10d js qs ks as", sep=' ')) >
+            eval_hand(Card.from_str_list("9d 10s js qs ks", sep=' ')) >
+            eval_hand(Card.from_str_list("Ad 2s 3s 4s 5s", sep=' '))
+        )
