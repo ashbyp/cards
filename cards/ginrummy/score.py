ff5c8805b8f597c11007768dc1c94bc0c24751ad
ashbyp <ashbyp@yahoo.co.uk>
2022-11-02 19:13:49 +0000

    fix get_melds

--- cards/ginrummy/score.py
+++ cards/ginrummy/score.py
@@ -5,15 +5,15 @@ def find_all_melds(hand):
     melds = []
     three = three_of_a_kind(hand)
     if three:
-        melds += three
+        melds.extend(three)
         four = four_of_a_kind(hand)
         if four:
-            melds += four
+            melds.extend(four)
 
     for run_len in range(3, len(hand) + 1):
         runs = same_suit_all_runs(hand, run_len)
         if runs:
-            melds += runs
+            melds.extend(runs)
         else:
             break
 
@@ -24,6 +24,10 @@ def score_hand(hand):
     if len(hand) not in (10, 11):
         raise ValueError('length of hand must be 10 or 11 cards')
 
-    # return a list of melds with the lowest possible deadwood count
-    return [], sum(x.value for x in hand)
+    all_melds = find_all_melds(hand)
+    if not all_melds:
+        return [], sum(x.value for x in hand)
+
+    return all_melds, 0 # TODO what algo here
+
 
